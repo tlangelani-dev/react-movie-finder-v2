fb2ae6b48368db554ab7682e06add7cac50dcea4
Tlangelani Mboweni <mbowenit@gmail.com>
2021-01-13 23:16:32 +0200

    update search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Search = () => {
+const Search = ({handleInput, search}) => {
     return (
         <div className="search-cp">
-            <input type="text" placeholder="Find a movie..." />
+            <input 
+                type="text"
+                placeholder="Find a movie..."
+                onChange={handleInput}
+                onKeyPress={search}
+            />
         </div>
     )
 }
