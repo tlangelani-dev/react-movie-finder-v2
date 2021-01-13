264779949f699bc9c76b20574e95d3ba593906cd
Tlangelani Mboweni <mbowenit@gmail.com>
2021-01-13 20:39:01 +0200

    add search component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Search from './Search';
 
 const App = () => {
     return (
-        <div className="app">
+        <div className="app-cp">
             <header>
                 <h1>Movie Finder <small>by Tlangelani</small></h1>
             </header>
+            <main>
+                <Search />
+            </main>
         </div>
     )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className="search-cp">
+            <input type="text" placeholder="Find a movie..." />
+        </div>
+    )
+}
+
+export default Search;
